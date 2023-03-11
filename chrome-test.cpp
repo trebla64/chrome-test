2057ecb2645ec809fddf2d2c0e169d185ee08a27
Albert Minnie <albert.minnie@gmail.com>
2023-03-11 15:17:01 +0200

    add base files

---
+++ chrome-test.cpp
@@ -0,0 +1,12 @@
+﻿// chrome-test.cpp : Defines the entry point for the application.
+//
+
+#include "chrome-test.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
